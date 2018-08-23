e1ab34476eb1297c8493dec1aa368f6f2852a0e3
maxnawa31 <mnawa31@gmail.com>
2018-08-22 19:27:51 -0700

    jwt working

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,43 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const SECRET = "askdfjsldkfjs";
+
+router.post("/login", async function(req,res,next) {
+  try{
+  const foundUser = await db.query(
+    "SELECT * FROM users WHERE email=$1 LIMIT 1",
+    [req.body.email]
+  );
+  if(foundUser.rows.length  === 0) {
+    return res.json({message: "Invalid Email"})
+  }
+  const hashedPassword = await bcrypt.compare(
+    req.body.password,
+    foundUser.rows[0].password
+  )
+  if(hashedPassword === false) {
+    return res.json({message: "Invalid Password"});
+  }
+
+  const token = jwt.sign(
+    {username: foundUser.rows[0].email},
+    SECRET,
+    {
+      expiresIn: 60 * 60
+    }
+  );
+  return res.json({token})
+  } catch(e) {
+    return res.json(e)
+  }
+});
+
+
+
+
 router.get("/", async function(req,res,next) {
   try{
     const results = await db.query("SELECT * FROM users");
